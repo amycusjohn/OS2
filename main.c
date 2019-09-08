1b779dfd70e1257bd41701dd0bee80c14169caa9
Amy Seidel <acs3k8@mail.umsl.edu>
2019-09-08 17:42:04 -0500

    Pretty much done, no errors

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 //Amy Seidel
 //CS4760 - OS 
 //Project 1 
+#include <time.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdlib.h>
@@ -10,13 +11,19 @@
 #include <errno.h>
 #include <dirent.h>
 #include <grp.h>
+void printdir(char *dir, int indent);
+
+//making all of the flags global so they can be changed in main and used in the function
+int hflag,tflag, gflag, pflag, iflag, uflag, sflag, Lflag, dflag,  Iflag = 0;
+int n = 4;
+
 int main( int argc, char *argv[] )  {
 	
+	//variables 
 	int var;
-	int hflag,lflag,  Lflag, dflag,  Iflag = 0;
-	int nflag = 0;
-	int n = 4;
-	while ((var = getopt(argc, argv, "hIdL")) != -1) { 
+
+	//while loop to execute getopt
+	while ((var = getopt(argc, argv, "hILldgipstu")) != -1) { 
 		switch(var) {
 		  case 'h':
 		   	hflag = 1;
@@ -30,84 +37,129 @@ int main( int argc, char *argv[] )  {
 		  case 'L':
 			Lflag = 1;
 			break;
+		  case 't':
+			tflag = 1;
+			break;
+		  case 'p': 
+			pflag = 1;
+			break;
+		  case 'i':
+			iflag = 1;
+			break;
+		  case 'u':
+			uflag = 1;
+			break;
+		  case 'g':
+			gflag = 1;
+			break;
+		  case 's':
+			sflag = 1;
+			break;
+		  case 'l':
+			uflag = 1;
+			tflag = 1;
+			iflag = 1;
+			pflag = 1;
+			gflag = 1;
+			sflag = 1;
 		   default:
-		 	lflag = 1; 
-			hflag = 1;
 			break;
 		}
 	}
-	
-	if(argv[optind] == NULL){
-		printdir(".",0);
-	}
-	for(; optind < argc; optind++){      
-       	//	tree(argv[optind], 4);
-    	} 
 	if((hflag ==1)) {
-		printf("This is a help message\n");
+		printf("HELP: Enter ./dt -options directory name\n");
 		exit(0);
 	}
-	if((Iflag == 1)){
+	if(argv[optind] != NULL){
+		printdir(argv[optind],0);
+	
 	}
-	if((Lflag == 1)) {
-		
+	//prints current directory if there is no argument provided
+	else{
+		printdir(".", 0);
 	}
-	if((dflag == 1)) {
-		
-	}
+    	 
 }
+/*****************************************
+ * FUNCTION TO PRINT DIRECTORY AND OPTIONS
+ *****************************************/
 
-
-void printdir(char *dir, int depth)
+void printdir(char *dir, int indent)
 {
-    DIR *dp;
+    //variables for declaring directory and stat
+    DIR *d;
     struct dirent *entry;
     struct stat statbuf;
-    int spaces = depth*4;
+    int spaces = indent+n;
 	
     //Vars for options
     char *p = getenv("USER");
-    int gid = (atoi(dir));
-    struct stat mystat; 
+    int gid = (atoi(dir)); 
     struct group *grp = getgrgid(gid);
     char buf[512];
-    
-
-
-   /* if(NULL == (grp = getgrgid(mystat.st_gid)))
-	{
-	  perror("getpwuid()");
-	}
-*/
-
-    if((dp = opendir(dir)) == NULL) {
-        fprintf(stderr,"cannot open directory: %s\n", dir);
+   
+   //Error checking if it is a real directory name or not
+    if((d = opendir(dir)) == NULL) {
+	fprintf(stderr, "ERRO %s: ", dir);
+	perror(""); 
         return;
     }
+
     chdir(dir);
-    while((entry = readdir(dp)) != NULL) {
+    while((entry = readdir(d)) != NULL) {
         lstat(entry->d_name,&statbuf);
-        if(S_ISDIR(statbuf.st_mode)) {
-            /* Found a directory, but ignore . and .. */
+        if(S_ISDIR(statbuf.st_mode))
+	 {
+            // ignoring "." and ".." directories
             if(strcmp(".",entry->d_name) == 0 || 
                 strcmp("..",entry->d_name) == 0 )
                 continue;
 	    
-            printf("%*-4s%s/",spaces,"",entry->d_name);
-       	    stat(buf, &mystat);
-     	    printf(" %-8s", entry->d_name);
-	    printf(" %-8zu",mystat.st_size);
-	    printf(" %-8s", p);
-            if(grp){
-	 	 printf(" %-8s", grp->gr_name);
-		}
-            /* Recurse at a new indent level */
-	    printf(" %-8x\n", mystat.st_mode);
-            printdir(entry->d_name,depth+1);
+	//Printing information
+            printf("%s",entry->d_name);
+     	
+	    //print permissions
+	    if(pflag == 1){
+		printf("\t");
+		printf( (S_ISDIR(statbuf.st_mode)) ? "d" : "-");	  	
+		printf( (statbuf.st_mode & S_IRUSR) ? "r" : "-");
+		printf( (statbuf.st_mode & S_IWUSR) ? "w" : "-");
+		printf( (statbuf.st_mode & S_IXUSR) ? "x" : "-");
+		printf( (statbuf.st_mode & S_IRGRP) ? "r" : "-");
+		printf( (statbuf.st_mode & S_IWGRP) ? "w" : "-");
+		printf( (statbuf.st_mode & S_IXGRP) ? "x" : "-");
+		printf( (statbuf.st_mode & S_IROTH) ? "r" : "-");
+		printf( (statbuf.st_mode & S_IWOTH) ? "w" : "-");
+		printf( (statbuf.st_mode & S_IXOTH) ? "x" : "-");
+		
+	    }
+            //print number of links
+            if(iflag == 1){
+	  	  printf("\t%d", statbuf.st_ino);
+	    }
+      	    //print UID
+	    if(uflag == 1){ 
+	   	 printf("\t%s", p);
+	    }	
+           //print group id
+           if(gflag == 1){ 
+		   if(grp){
+		 	 printf("\t%s", grp->gr_name);
+			}
+	   }
+	   if(sflag == 1){		
+	     printf("\t%d",statbuf.st_size);
+	   }
+
+	   //printing time
+	   if(dflag == 1){
+	    printf("\t%s\n", ctime(&statbuf.st_mtime));
+	  } 
+	   printdir(entry->d_name, indent+n); 
         }
-        else printf("%*s%s\n",spaces,"",entry->d_name);
+        else printf("%*s%s\n ",spaces,"",entry->d_name);
     }
     chdir("..");
-    closedir(dp);
+    closedir(d);
 }
 
